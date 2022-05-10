66aa9bb5685da636947f440e0f408e9f91db4cae
hakeem-jc <hclarke.dev@gmail.com>
2022-05-10 15:20:19 -0500

    Create button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './Styles/App.css';
 import { Navbar } from './components/Navbar/navbar';
+import { Content } from './components/Content/content';
 import { Footer } from './components/Footer/footer';
 
 function App() {
   return (
     <section className='app-background'>
         <Navbar/>
-        
+        <Content/>
         <Footer />
     </section>
   );
