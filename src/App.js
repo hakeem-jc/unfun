3567c218d124d55a14bce7145583ee0ea6235f59
hakeem-jc <hclarke.dev@gmail.com>
2022-05-10 14:40:32 -0500

    Create background and header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import './Styles/App.css';
+import { Navbar } from './components/Navbar/navbar';
+
 function App() {
   return (
-  <></>
+    <section className='app-background'>
+        <Navbar/>
+    </section>
   );
 }
 
