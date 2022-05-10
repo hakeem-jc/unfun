3de980a51043196af0713dce78f9fe27e8a4c03b
hakeem-jc <hclarke.dev@gmail.com>
2022-05-10 14:51:48 -0500

    Add footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './Styles/App.css';
 import { Navbar } from './components/Navbar/navbar';
+import { Footer } from './components/Footer/footer';
 
 function App() {
   return (
     <section className='app-background'>
         <Navbar/>
+        
+        <Footer />
     </section>
   );
 }
