2872bb2befce7ac7c1e305e354476d53854e9162
VEN-ClarkeHaJ <clarkehaj@jncb.com>
2022-05-10 14:40:32 -0500

    Create background and header

---
+++ src/components/Navbar/navbar.jsx
@@ -0,0 +1,8 @@
+import './navbar.css';
+
+export function Navbar () {
+    return <header className="header">
+        <a href="/" className="logo">UNFUN</a>
+        <h1 className="author-logo">MadeByHC</h1>
+    </header>
+}
