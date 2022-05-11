71d7b2aa6bf97e5d775ec5a25b497eb772a1932f
hakeem-jc <hclarke.dev@gmail.com>
2022-05-11 10:38:45 -0500

    Edit hero text

--- src/components/Content/content.jsx
+++ src/components/Content/content.jsx
@@ -3,7 +3,7 @@ import './content.css';
 export function Content() {
     return (
         <section className="content-container">
-            <h1 className="content-text">Feeling too good? Let's fix that with demotivational quotes</h1>
+            <h1 className="content-text">Feeling too good? Let's fix that with some demotivational quotes</h1>
             <button className='content-btn'>Let's Go</button>
         </section>
     )
