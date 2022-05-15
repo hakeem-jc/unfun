bd18c6b75aae4d2f7c3261f516ad18cfc25a164d
hakeem-jc <hclarke.dev@gmail.com>
2022-05-15 05:02:28 -0500

    Update style

--- src/components/Content/content.jsx
+++ src/components/Content/content.jsx
@@ -5,14 +5,18 @@ import './content.css';
 let url = 'http://127.0.0.1:4000';
 
 export const Content = () => {
-
+    // Button Text
     const starterText = "Let's Go";
     const [btnText, setBtnText] = useState(starterText);
+
+    // Dynamic Content
     let [content, setContent] = useState({
         id:"",
         quote:"Feeling too good? Let's fix that with some demotivational quotes",
-        author:"Test"
+        author:""
     });
+
+    // Set current quote
     const setQuote = () => {
         if (btnText === starterText) setBtnText("Next");
         setContent(fetchQuote());
@@ -21,11 +25,10 @@ export const Content = () => {
     return (
         <>
            <section className="content-container">
-                <h1 className="content-text">{content.quote}</h1>
+                <h1 className="content-header">{content.quote}</h1>
                 <h2 className="content-sub-header">{content.author}</h2>
                 <button className='content-btn' onClick={()=>setQuote()}>{btnText}</button>
             </section>
         </>
-        
     )
 }
