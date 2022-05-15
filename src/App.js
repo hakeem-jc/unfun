ce364991314a148409770c14c8e683fb1322d14d
VEN-ClarkeHaJ <clarkehaj@jncb.com>
2022-05-15 08:18:38 -0500

    Add Errror Boundaries

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import './Styles/App.css';
 import Landing from './pages/Landing';
 import PageNotFound from './pages/PageNotFound';
+import ErrorFallback from './pages/ErrorFallback';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ErrorBoundary } from 'react-error-boundary';
 
 function App() {
   return (
-   <Router>
-     <Routes>
-      <Route path='/' element={<Landing />}/>
-      <Route path='*' element={<PageNotFound />}/>
-     </Routes>
-   </Router>
+    <ErrorBoundary
+      FallbackComponent={ErrorFallback}
+    >
+      <Router>
+        <Routes>
+          <Route path='/' element={<Landing />}/>
+          <Route path='*' element={<PageNotFound />}/>
+        </Routes>
+      </Router>
+    </ErrorBoundary>
   );
 }
 
