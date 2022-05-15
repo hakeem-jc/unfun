bafab80cd7628ddb121947e11d3a818bb321166d
hakeem-jc <hclarke.dev@gmail.com>
2022-05-15 04:46:43 -0500

    Change quote on button click

--- src/components/Content/content.jsx
+++ src/components/Content/content.jsx
@@ -1,10 +1,31 @@
+import { useState } from 'react';
+import { fetchQuote } from './content-logic';
 import './content.css';
 
-export function Content() {
+let url = 'http://127.0.0.1:4000';
+
+export const Content = () => {
+
+    const starterText = "Let's Go";
+    const [btnText, setBtnText] = useState(starterText);
+    let [content, setContent] = useState({
+        id:"",
+        quote:"Feeling too good? Let's fix that with some demotivational quotes",
+        author:"Test"
+    });
+    const setQuote = () => {
+        if (btnText === starterText) setBtnText("Next");
+        setContent(fetchQuote());
+    }
+
     return (
-        <section className="content-container">
-            <h1 className="content-text">Feeling too good? Let's fix that with some demotivational quotes</h1>
-            <button className='content-btn'>Let's Go</button>
-        </section>
+        <>
+           <section className="content-container">
+                <h1 className="content-text">{content.quote}</h1>
+                <h2 className="content-sub-header">{content.author}</h2>
+                <button className='content-btn' onClick={()=>setQuote()}>{btnText}</button>
+            </section>
+        </>
+        
     )
 }
