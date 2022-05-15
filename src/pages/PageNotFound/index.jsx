7da483cafb7c60642323f9ed72023fa68d0f091d
hakeem-jc <hclarke.dev@gmail.com>
2022-05-15 08:32:49 -0500

    Refactored 404 Page

--- src/pages/PageNotFound/index.jsx
+++ src/pages/PageNotFound/index.jsx
@@ -1,18 +1,13 @@
-import { Navbar } from "../../components/Navbar/navbar";
-import { Footer } from "../../components/Footer/footer";
-import {Link } from 'react-router-dom';
+import { Container } from "../../components/Container";
+import { Link } from 'react-router-dom';
 
-function PageNotFound() {
+export const PageNotFound = () => {
     return (
-      <section className='app-background'>
-          <Navbar/>
+        <Container>
           <section className="content-container">
               <h1 className="content-header">Page Not Found</h1>
               <Link to='/' className='content-btn'>Go Home</Link>
           </section>
-          <Footer />
-      </section>
+        </Container>
     );
-  }
-  
-  export default PageNotFound;
+}
