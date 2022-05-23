385b1709440e9d91817ea345077215180ffd0a84
VEN-ClarkeHaJ <clarkehaj@jncb.com>
2022-05-23 15:54:44 -0500

    Add static localization menu

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -3,7 +3,11 @@ import './footer.css';
 export function Footer() {
     return (
         <footer>
-            <p>&copy; 2022 Hakeem Clarke</p>
+            <nav className='footer-language'>
+                <h2 className='footer-language-option'>EN</h2>
+                <h2 className='footer-language-option'>ES</h2>
+            </nav>
+            <p className="footer-copyright">&copy; 2022 Hakeem Clarke</p>
         </footer>
     )
 }
